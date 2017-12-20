d06f14b6a510972e400cb9dd934c1eaa0378e091
Tignite <timo.cramer@uni-bonn.de>
2017-12-20 23:46:04 +0100

    Update main.cpp

--- Abgabe5/main.cpp
+++ Abgabe5/main.cpp
@@ -1,5 +1,6 @@
 #include "input.h"
 #include "graphen.h"
+#include "GraphenList.h"
 
 int ** sum(int **mat1 , int **mat2 , int n , int m);
 int ** product(int  **mat1 , int  **mat2 ,
@@ -8,37 +9,48 @@ int n1 , int m1 , int n2 , int m2);
 
 int main() {
 
-	Graphen *G = new Graphen("graph1.dat");
-	G->print();
-
-	G->wegExist(10, 0, 3);
-	//G->kreisExist();
 	/*
-int **matrix1, **matrix2, **matrix3, **matrixErgebnis, n1, m1, n2, m2;
-//Einlesen der Matrix aus graph1.dat
-matrix1 = read_matrix_file("graph1.dat", &n1, &m1);
-matrix2 = read_matrix_file("graph2.dat", &n2, &m2);
-matrix3 = read_matrix_file("graph2.dat", &n2, &m2);
-//Ausgeben der Matrix in der Konsole
-//cout<<"Matrix 1:\n";
-//print_matrix(matrix1,n1,m1);
-cout<<"Matrix 2:\n";
-print_matrix(matrix2,n2,m2);
-matrixErgebnis = sum(matrix3, matrix2, n2, m2);
-cout<<"Matrix Summe:\n";
-print_matrix(matrixErgebnis,n2,m2);
+	int **matrix1, **matrix2, **matrix3, **matrixErgebnis, n1, m1, n2, m2;
+	//Einlesen der Matrix aus graph1.dat
+	matrix1 = read_matrix_file("graph1.dat", &n1, &m1);
+	matrix2 = read_matrix_file("graph2.dat", &n2, &m2);
+	matrix3 = read_matrix_file("graph2.dat", &n2, &m2);
+	//Ausgeben der Matrix in der Konsole
+	//cout<<"Matrix 1:\n";
+	//print_matrix(matrix1,n1,m1);
+	cout<<"Matrix 2:\n";
+	print_matrix(matrix2,n2,m2);
+	matrixErgebnis = sum(matrix3, matrix2, n2, m2);
+	cout<<"Matrix Summe:\n";
+	print_matrix(matrixErgebnis,n2,m2);
 
-delete_matrix(matrixErgebnis, n2, m2);
-matrixErgebnis = product(matrix2, matrix2, n2, m2, n2, m2);
-cout<<"Matrix Product:\n";
-print_matrix(matrixErgebnis,n2,m2);
+	delete_matrix(matrixErgebnis, n2, m2);
+	matrixErgebnis = product(matrix2, matrix2, n2, m2, n2, m2);
+	cout<<"Matrix Product:\n";
+	print_matrix(matrixErgebnis,n2,m2);
 
 
-delete_matrix(matrix1, n1, m1);
-delete_matrix(matrix2, n2, m2);
-delete_matrix(matrix3, n2, m2);
-delete_matrix(matrixErgebnis, n2, m2);
-*/
+	delete_matrix(matrix1, n1, m1);
+	delete_matrix(matrix2, n2, m2);
+	delete_matrix(matrix3, n2, m2);
+	delete_matrix(matrixErgebnis, n2, m2);
+	return 0;
+
+	Aufgabe 2
+	*/
+	Graphen *G1 = new Graphen("graph1.dat");
+	Graphen *G2 = new Graphen("graph2.dat");
+	G1->print();
+	G1->wegExist(10, 0, 3);
+	G1->kreisExist();
+	G1->zusammenhang();
+	G2->print();
+	G2->wegExist(10, 4, 3);
+	G2->kreisExist();
+	G2->zusammenhang();
+	delete G1;
+	delete G2;
+	return 0;
 }
 
 int ** sum(int **mat1 , int **mat2 , int n , int m) {
